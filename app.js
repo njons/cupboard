94054fad6d142ead8ac0798fb48ea3b32e6921f0
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2019-03-16 17:07:54 +0100

    adds static .css file to server

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
+
 app.listen(6000, () => {
   console.log("server is up and running on port 6000");
 });
